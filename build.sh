bdeb7c6253ba4c30c9603052b5c99c33f1dbc38d
Ajinkya Sonawane <ajinkyasonawane@10-18-81-212.dynapool.wireless.nyu.edu>
2021-12-13 20:12:08 -0500

    Update build.sh for optimized compilation and Add Project Report

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 gcc -O3 Part1/part_1.c -o run
-gcc Part2/part_2.c -o run2
-gcc Part3/part_3.c -o run3
-gcc Part4/part_4.c -o run4
-gcc Part5/part_5.c -o run5
-gcc Part6/part_6.c -o run6
+gcc -O3 Part2/part_2.c -o run2
+gcc -O3 Part3/part_3.c -o run3
+gcc -O3 Part4/part_4.c -o run4
+gcc -O3 Part5/part_5.c -o run5
+gcc -O3 Part6/part_6.c -o run6
 gcc -O3 fast.c -o fast
